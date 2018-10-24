a8b2faeb16f4df2e11e74e7ca6d5fc9e2d840a39
Richard Allen White III <raw937@gmail.com>
2018-10-24 13:49:11 +0200

    Add files via upload

---
+++ bin/day4/prokka.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+prokka 255-15.fasta --outdir 255-15 --genus Exiguobacterium --prefix 255-15
+prokka 7-3.fasta --outdir 7-3 --genus Exiguobacterium --prefix 7-3
+prokka GIC31.fasta --outdir GIC31 --genus Exiguobacterium --prefix GIC31
+prokka N139.fasta --outdir N139 --genus Exiguobacterium --prefix N139
+prokka RW2.fasta --outdir RW2 --genus Exiguobacterium --prefix RW2
+
